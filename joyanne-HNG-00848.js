c8f1a5d57c10fd45b5e8d92f1dd4ae017736e127
Joyanne2020 <66220004+Joyanne2020@users.noreply.github.com>
2020-06-05 11:12:51 +0100

    Add files via upload

--- joyanne-HNG-00848.js
+++ joyanne-HNG-00848.js
@@ -1,6 +1,6 @@
-let fullName = "Joy Ogu"
+let name = "Joy Ogu"
 let id = "HNG-00848"
-let email = "joyogu022@gmail.com"
+let email = "joyogu022@gmail.com
 let language = "Javascript"
-let result = "Hello word" + "this is " + fullName + " with " + id + "and" + email + "using" + language + "for stage 2 task"
+let result ="Hello world, " + "this is " + name + "with " + "HNGi7 ID " + id + "and " + "email " + email + " using " + language + "for stage 2 task "
 console.log(result)
